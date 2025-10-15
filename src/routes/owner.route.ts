a18dca15983caab3bfd7c72ed54c78a41498ac6c
italo <italoteles399@gmail.com>
2025-10-15 10:50:09 -0300

    adicionado rota de atualizar usuario

--- src/routes/owner.route.ts
+++ src/routes/owner.route.ts
@@ -2,11 +2,17 @@ import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { OwnerController } from "../controllers/owner.controller";
 import { celebrate, Segments } from "celebrate";
-import { deleteAccountSchema } from "../models/owner.model";
+import { deleteAccountSchema, updateOwnerSchema } from "../models/owner.model";
 
 export const ownerRoutes = Router();
 
 ownerRoutes.get("/", authMiddleware, OwnerController.getAll);
+ownerRoutes.patch(
+    "/update-account",
+    authMiddleware,
+    celebrate({ [Segments.BODY]: updateOwnerSchema }),
+    OwnerController.update
+);
 ownerRoutes.delete(
     "/delete-account",
     authMiddleware,
