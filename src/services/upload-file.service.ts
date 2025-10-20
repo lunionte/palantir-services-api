6558c482133ebe0c08963fbec7ed8747f627f63d
italo <italoteles399@gmail.com>
2025-10-20 09:13:50 -0300

    adicionado serviço de upload de imagem de base64

---
+++ src/services/upload-file.service.ts
@@ -0,0 +1,37 @@
+import { createClient } from "@supabase/supabase-js";
+import { fileTypeFromBuffer } from "file-type";
+import { ValidationError } from "../errors/validation.error";
+import { randomUUID } from "crypto";
+import { InternalServerError } from "../errors/internal-server.error";
+
+const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_KEY!);
+
+export class UploadFileService {
+    constructor(private path: string = "") {}
+
+    async upload(base64: string) {
+        // tranforma a string base64 recebida para binarios
+        const fileBuffer = Buffer.from(base64, "base64");
+
+        const fileType = await fileTypeFromBuffer(fileBuffer);
+
+        if (fileType?.mime !== "image/png" && fileType?.mime !== "image/jpeg") {
+            throw new ValidationError("A extensão do arquivo é inválida");
+        }
+
+        if (!fileType) {
+            throw new ValidationError("A extensão do arquivo é inválida");
+        }
+
+        const fileName = `${this.path}${randomUUID()}.${fileType.ext}`;
+
+        const { error } = await supabase.storage.from("business").upload(fileName, fileBuffer);
+
+        if (error) {
+            throw new InternalServerError("Ocorreu um erro ao fazer upload da imagem");
+        }
+
+        const { data } = await supabase.storage.from("business").getPublicUrl(fileName);
+        return data.publicUrl;
+    }
+}
