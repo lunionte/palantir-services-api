96ed0442e4bb8f62abfe36b6c6a7af125df83813
italo <italoteles399@gmail.com>
2025-10-20 09:37:44 -0300

    adicionado service para crud de business

---
+++ src/services/business.service.ts
@@ -0,0 +1,58 @@
+import { ForbidenError } from "../errors/forbiden.error";
+import { NotFoundError } from "../errors/not-found.error";
+import { IBusiness } from "../models/business.model";
+import { BusinessRepository } from "../repositories/business.repository";
+import { UploadFileService } from "./upload-file.service";
+
+export class BusinessService {
+    private businessRepository;
+    private uploadFileService;
+    constructor() {
+        this.businessRepository = new BusinessRepository();
+        this.uploadFileService = new UploadFileService("images/");
+    }
+    async getAll() {
+        const data = await this.businessRepository.getAll();
+        return data;
+    }
+
+    async getById(businessId: string) {
+        const data = await this.businessRepository.getById(businessId);
+        return data;
+    }
+
+    async create(ownerId: string, business: IBusiness) {
+        if (business.logo) {
+            const logo = await this.uploadFileService.upload(business.logo);
+            business.logo = logo;
+        }
+
+        business.ownerId = ownerId;
+        const data = await this.businessRepository.save(business);
+        return data;
+    }
+
+    async update(businessId: string, business: IBusiness) {
+        if (business.logo) {
+            const logo = await this.uploadFileService.upload(business.logo);
+            business.logo = logo;
+        }
+        const data = await this.businessRepository.update(businessId, business);
+        return data;
+    }
+
+    async delete(businessId: string, ownerId: string) {
+        const business = await this.businessRepository.getBusinessOwnerById(businessId);
+
+        if (!business) {
+            throw new NotFoundError("Business não encontrado");
+        }
+
+        if (business?.ownerId !== ownerId) {
+            throw new ForbidenError();
+        }
+
+        const data = await this.businessRepository.delete(businessId);
+        return data;
+    }
+}
