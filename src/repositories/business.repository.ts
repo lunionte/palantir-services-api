c23a9a347d9a268f5b69dce4eb7709ee21129ff4
italo <italoteles399@gmail.com>
2025-10-20 09:38:04 -0300

    adicionado repository para crud de business

---
+++ src/repositories/business.repository.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+import { IBusiness } from "../models/business.model";
+
+const prisma = new PrismaClient();
+
+export class BusinessRepository {
+    async getAll() {
+        const data = await prisma.business.findMany();
+        return data;
+    }
+
+    async getById(id: string) {
+        const data = await prisma.business.findUnique({
+            where: { id },
+        });
+        return data;
+    }
+
+    async getBusinessOwnerById(id: string) {
+        const data = await prisma.business.findUnique({
+            where: { id },
+            select: { ownerId: true },
+        });
+        return data;
+    }
+
+    async save(business: IBusiness) {
+        const data = await prisma.business.create({
+            data: business,
+        });
+        return data;
+    }
+
+    async update(id: string, business: IBusiness) {
+        const data = await prisma.business.update({
+            where: { id },
+            data: business,
+        });
+        return data;
+    }
+
+    async delete(id: string) {
+        await prisma.business.delete({
+            where: { id },
+        });
+        return;
+    }
+}
