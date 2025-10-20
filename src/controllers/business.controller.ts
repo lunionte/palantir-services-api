993710e7e57620eb79013582d82c976bedf82a8a
italo <italoteles399@gmail.com>
2025-10-20 09:37:39 -0300

    adicionado controller para crud de business

---
+++ src/controllers/business.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { IBusiness } from "../models/business.model";
+import { BusinessService } from "../services/business.service";
+
+export class BusinessController {
+    static async getAll(req: Request, res: Response) {
+        const data = await new BusinessService().getAll();
+        res.json({ data });
+    }
+
+    static async getById(req: Request, res: Response) {
+        const { id } = req.params;
+        const data = await new BusinessService().getById(id);
+        res.json({ data });
+    }
+
+    static async create(req: Request, res: Response) {
+        const business = req.body as IBusiness;
+        const ownerId = req.user.id;
+        const data = await new BusinessService().create(ownerId, business);
+        res.status(201).json({ data });
+    }
+
+    static async update(req: Request, res: Response) {
+        const { id } = req.params;
+        const business = req.body as IBusiness;
+        const data = await new BusinessService().update(id, business);
+        res.json({ data });
+    }
+
+    static async delete(req: Request, res: Response) {
+        const { id } = req.params;
+        const ownerId = req.user.id;
+        const data = await new BusinessService().delete(id, ownerId);
+        res.json({ data });
+    }
+}
