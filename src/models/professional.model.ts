0eb38d037bb227eed03d455e993fa15e4c5de6ff
italo <italoteles399@gmail.com>
2025-10-22 08:07:04 -0300

    adicionado modelo de funcionarios

---
+++ src/models/professional.model.ts
@@ -0,0 +1,18 @@
+import { Joi } from "celebrate";
+
+export interface IProfessional {
+    name: string;
+    email: string;
+    password: string;
+    phone?: string;
+    businessId: string;
+    createdAt?: string;
+}
+
+export const newProfessionalSchema = Joi.object().keys({
+    name: Joi.string().max(50).trim().required(),
+    email: Joi.string().email().trim().required(),
+    password: Joi.string().min(6).max(15).trim().required(),
+    phone: Joi.string().length(11).trim().optional(),
+    businessId: Joi.string().length(24).trim().required(),
+});
