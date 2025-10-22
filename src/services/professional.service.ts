c50eb16f818432cc7bb5a2eb5e3d02af02ffc11b
italo <italoteles399@gmail.com>
2025-10-22 12:05:02 -0300

    adicionado função de getbyid no service e signin

--- src/services/professional.service.ts
+++ src/services/professional.service.ts
@@ -4,6 +4,7 @@ import { IProfessional } from "../models/professional.model";
 import bcrypt from "bcrypt";
 import { BusinessRepository } from "../repositories/business.repository";
 import { ProfessionalRepository } from "../repositories/professional.repository";
+import jwt from "jsonwebtoken";
 
 export class ProfessionalService {
     private businessRepository;
@@ -12,6 +13,11 @@ export class ProfessionalService {
         this.businessRepository = new BusinessRepository();
         this.professionalRepository = new ProfessionalRepository();
     }
+    async getById(id: string) {
+        const data = await this.professionalRepository.getById(id);
+        return data;
+    }
+
     async create(professional: IProfessional, id: string) {
         const business = await this.businessRepository.getById(professional.businessId);
         if (!business) {
@@ -29,5 +35,25 @@ export class ProfessionalService {
 
         const data = await this.professionalRepository.save(professional);
         return data;
+    }
+
+    async signIn(email: string, password: string) {
+        const professional = await this.professionalRepository.getByEmail(email);
+        if (!professional) {
+            throw new ValidationError("Email ou senha inválidos");
+        }
+
+        const isValidPassword = await bcrypt.compare(password, professional.password);
+
+        if (!isValidPassword) {
+            throw new ValidationError("Email ou senha inválidos");
+        }
+
+        const token = jwt.sign(
+            { id: professional.id, role: professional.role },
+            process.env.JWT_SECRET!,
+            { expiresIn: "7d" }
+        );
+        return token;
     }
 }
