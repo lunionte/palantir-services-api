86bebf8036346d575f5401a86c1e26e467f10b0d
italo <italoteles399@gmail.com>
2025-10-22 08:30:33 -0300

    adicionado repository de profissional

--- src/repositories/professional.repository.ts
+++ src/repositories/professional.repository.ts
@@ -4,6 +4,18 @@ import { IProfessional } from "../models/professional.model";
 const prisma = new PrismaClient();
 
 export class ProfessionalRepository {
+    async getAll() {
+        const data = await prisma.professional.findMany();
+        return data;
+    }
+
+    async getById(id: string) {
+        const data = await prisma.professional.findUnique({
+            where: { id },
+        });
+        return data;
+    }
+
     async save(professional: IProfessional) {
         const data = await prisma.professional.create({
             data: professional,
