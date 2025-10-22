c3c1f7c02981f8510bc1a94cbb506acf7a9ca49a
Italo <italoteles399@gmail.com>
2025-10-22 04:03:50 -0300

    removido lançamento de erro em caso de prismaclient para evitar conflitos

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { ErrorBase } from "../errors/base.error";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-import { ValidationError } from "../errors/validation.error";
+
 import { InternalServerError } from "../errors/internal-server.error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -9,8 +8,6 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
 
     if (err instanceof ErrorBase) {
         err.json(res);
-    } else if (err instanceof PrismaClientKnownRequestError) {
-        throw new ValidationError("O Email já existe");
     } else {
         throw new InternalServerError();
     }
