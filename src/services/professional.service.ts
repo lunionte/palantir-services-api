da28df43c921fac46877d351868fb34bf10adf52
Italo <italoteles399@gmail.com>
2025-10-23 09:27:09 -0300

    adicionado função de getAll e possibilidade de acionar imagem para o profissional no create

--- src/services/professional.service.ts
+++ src/services/professional.service.ts
@@ -5,19 +5,29 @@ import bcrypt from "bcrypt";
 import { BusinessRepository } from "../repositories/business.repository";
 import { ProfessionalRepository } from "../repositories/professional.repository";
 import jwt from "jsonwebtoken";
+import { UploadFileService } from "./upload-file.service";
 
 export class ProfessionalService {
     private businessRepository;
     private professionalRepository;
+    private uploadFileService;
     constructor() {
         this.businessRepository = new BusinessRepository();
         this.professionalRepository = new ProfessionalRepository();
+        this.uploadFileService = new UploadFileService();
     }
+
+    async getAll() {
+        const data = await this.professionalRepository.getAll();
+        return data;
+    }
+
     async getById(id: string) {
         const data = await this.professionalRepository.getById(id);
         return data;
     }
 
+    // usado em business
     async create(professional: IProfessional, id: string) {
         const business = await this.businessRepository.getById(professional.businessId);
         if (!business) {
@@ -25,9 +35,12 @@ export class ProfessionalService {
         }
 
         if (business.ownerId !== id) {
-            throw new ValidationError(
-                "Você não tem permisão para adicionar funcionários neste business"
-            );
+            throw new ValidationError("Você não tem permisão para adicionar funcionários neste business");
+        }
+
+        if (professional.image) {
+            const image = await this.uploadFileService.upload(professional.image);
+            professional.image = image;
         }
 
         const hashedPassword = await bcrypt.hash(professional.password, 10);
@@ -49,11 +62,9 @@ export class ProfessionalService {
             throw new ValidationError("Email ou senha inválidos");
         }
 
-        const token = jwt.sign(
-            { id: professional.id, role: professional.role },
-            process.env.JWT_SECRET!,
-            { expiresIn: "7d" }
-        );
+        const token = jwt.sign({ id: professional.id, role: professional.role }, process.env.JWT_SECRET!, {
+            expiresIn: "7d",
+        });
         return token;
     }
 }
