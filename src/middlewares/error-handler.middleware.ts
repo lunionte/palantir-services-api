ffbe6c27d2796825785a7dda4fa0a5614165b72a
italo <italoteles399@gmail.com>
2025-10-24 09:31:36 -0300

    adicionado handler de erro do prisma de email já criado

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -2,12 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import { ErrorBase } from "../errors/base.error";
 
 import { InternalServerError } from "../errors/internal-server.error";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { ValidationError } from "../errors/validation.error";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(err);
 
     if (err instanceof ErrorBase) {
         err.json(res);
+    } else if (err instanceof PrismaClientKnownRequestError && err.code === "P2002") {
+        const validationError = new ValidationError("Email já em uso");
+        validationError.json(res);
     } else {
         throw new InternalServerError();
     }
