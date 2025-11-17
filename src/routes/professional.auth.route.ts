553fd8b81aee0cffdb83e1b874d8f1544e3e9b7c
italo <italoteles399@gmail.com>
2025-11-17 07:50:32 -0300

    adiiconado rota de auth de professional

--- src/routes/professional.auth.route.ts
+++ src/routes/professional.auth.route.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import { ProfessionalController } from "../controllers/professional.controller";
+import { celebrate, Segments } from "celebrate";
+import { newProfessionalLoginSchema } from "../models/professional.model";
 
 export const professionalRoutes = Router();
-professionalRoutes.post("/signIn", ProfessionalController.signIn);
+professionalRoutes.post(
+    "/signIn",
+    celebrate({ [Segments.BODY]: newProfessionalLoginSchema }),
+    ProfessionalController.signIn
+);
